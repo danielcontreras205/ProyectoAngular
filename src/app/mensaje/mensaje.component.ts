ea9ab9b96aeca297edd9619ba9b2aab4ce0754dd
danielcontreras205 <danielcontreras205@gmail.com>
2025-05-01 18:35:24 -0500

    ajuste del mensaje

--- src/app/mensaje/mensaje.component.ts
+++ src/app/mensaje/mensaje.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-mensaje',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule],
   templateUrl: './mensaje.component.html',
   styleUrl: './mensaje.component.css'
 })
